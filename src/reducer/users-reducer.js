fe941e9526f760961cfb629eaeff63e629a49651
kaur01 <40004799+kaur01@users.noreply.github.com>
2018-07-19 20:24:04 +0530

    display followers

--- src/reducer/users-reducer.js
+++ src/reducer/users-reducer.js
@@ -1,12 +1,16 @@
 export default function(state = { totalCount: 0, users: [] }, action ){
     switch(action.type){
         case "REFRESH-USERS":
-        state = {
-            totalCount: action.payload.totalCount,
-            users: action.payload.users
-        }
-        console.log({...state})
-        return {...state}
-        default: return {...state}
+                state = {
+                    totalCount: action.payload.totalCount,
+                    users: action.payload.users
+                }
+                 return {...state}
+
+        case "USER-DETAILS":
+                state.users[action.payload.index].followers = action.payload.followers;
+                return{...state}
+        default:
+                return {...state}
     }
  }
